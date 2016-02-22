6879b17f094af7e227cee3fe92579f7410fc6e3f
dmitriibarillo <dmitrybarillo@gmail.com>
2016-02-22 21:04:18 +0600

    add new connection between participant in the education process

--- Task_1/Internal/ViewController.m
+++ Task_1/Internal/ViewController.m
@@ -6,7 +6,6 @@
 #import "HeadOfDepartment.h"
 #import "DepartmentController.h"
 
-
 @interface ViewController ()
 
 @end
@@ -15,7 +14,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
     NSArray<Student *> *students =
         @[
           [[Student alloc] initWithFirstName:@"Jack" lastName:@"Smith" age:18],
@@ -27,7 +26,7 @@
           [[Student alloc] initWithFirstName:@"Daniel" lastName:@"Backer" age:30],
           [[Student alloc] initWithFirstName:@"Jason" lastName:@"Brown" age:28],
           [[Student alloc] initWithFirstName:@"Lily" lastName:@"Brooks" age:24]
-          ];
+        ];
         
         NSArray<Professor *> *professors =
         @[
@@ -36,13 +35,13 @@
           [[Professor alloc] initWithFirstName:@"Henry" lastName:@"Brooks" age:54],
           [[Professor alloc] initWithFirstName:@"Aria" lastName:@"Clifford" age:36],
           [[Professor alloc] initWithFirstName:@"Chloe" lastName:@"Crossman" age:40]
-          ];
+        ];
         
         NSArray<HeadOfDepartment *> *headOfDepartment =
         @[
           [[HeadOfDepartment alloc] initWithHeadOfDepartment:[professors objectAtIndex:0]],
           [[HeadOfDepartment alloc] initWithHeadOfDepartment:[professors objectAtIndex:1]]
-          ];
+        ];
         
         [[headOfDepartment objectAtIndex:0] addSubordinates:[professors objectAtIndex:2]];
         [[headOfDepartment objectAtIndex:0] addSubordinates:[professors objectAtIndex:3]];
@@ -53,7 +52,7 @@
         @[
           [[Department alloc] initWithTitle:@"Mathematic" headOfDepartment:[headOfDepartment objectAtIndex:0]],
           [[Department alloc] initWithTitle:@"Information Technology" headOfDepartment:[headOfDepartment objectAtIndex:1]]
-          ];
+        ];
         
         [[departments objectAtIndex:0] addProfessor:[professors objectAtIndex:0]];
         [[departments objectAtIndex:0] addProfessor:[professors objectAtIndex:1]];
@@ -67,35 +66,34 @@
           [[Group alloc] initWithTitle:@"1010"],
           [[Group alloc] initWithTitle:@"1011"],
           [[Group alloc] initWithTitle:@"1012"]
-          ];
+        ];
         
-        //[[groups objectAtIndex:0] addStudent:[students objectAtIndex:0]];
-        //[[departments objectAtIndex:0] addGroup:[groups objectAtIndex:0]];
-        //[[departments objectAtIndex:0] addGroup:[groups objectAtIndex:1]];
-        //[[departments objectAtIndex:1] addGroup:[groups objectAtIndex:2]];
-        //[[departments objectAtIndex:1] addGroup:[groups objectAtIndex:3]];
-        /*
+        [[groups objectAtIndex:0] addStudent:[students objectAtIndex:0]];
+        [[departments objectAtIndex:0] addGroup:[groups objectAtIndex:0]];
+        [[departments objectAtIndex:0] addGroup:[groups objectAtIndex:1]];
+        [[departments objectAtIndex:1] addGroup:[groups objectAtIndex:2]];
+        [[departments objectAtIndex:1] addGroup:[groups objectAtIndex:2]];
+    
          for (Student *student in students) {
-         for (int i = 0; i < 10; i++) {
-         int mark = arc4random_uniform(6);
-         [student addMark:[NSNumber numberWithInt:mark]];
-         }
-         int groupID = arc4random_uniform(3);
-         //[[groups objectAtIndex:groupID] addStudent:student];
-         
+             for (int i = 0; i < 10; i++) {
+                int mark = arc4random_uniform(6);
+                [student addMark:[NSNumber numberWithInt:mark]];
+             }
+             int groupID = arc4random_uniform(3);
+             [[groups objectAtIndex:groupID] addStudent:student];
          }
          
          DepartmentController *departmentController = [DepartmentController instance];
          
          for (Department *department in departments) {
-         [departmentController addDepartment:department];
+             [departmentController addDepartment:department];
          }
          
          for (Department *department in departmentController.departments) {
-         NSLog(@"%@", [department description]);
+             NSLog(@"%@", [department description]);
          }
          
-         */
+    
         
         NSLog(@"URA");
 }
